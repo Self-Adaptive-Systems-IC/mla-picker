7781b22c113ea81ce0b0f877dd1218aa3f258092
Romulo Soares <romulosoares@ice.ufjf.br>
2023-04-10 16:27:08 -0300

    Add comments to utils/cross_validation_utils.py

--- src/utils/cross_validation_utils.py
+++ src/utils/cross_validation_utils.py
@@ -17,30 +17,33 @@ def cross_validation(X_data, y_data):
         y_data : array
             The target variable to try predict in the case of supervised learning.
     """
-    n_size = 1000
-    results_tree = []
-    results_random_forest = []
-    results_svm = []
+    n_size = 5 # How much time will run the cross-validation
+    results_tree = [] # Save all results for Decision Tree model
+    results_random_forest = [] # Save all results fot Random Forest
+    results_svc = [] # Save all results for SVC
+    # Start loader -> Loading animation
     loader = Loader(
-            f"・ Running \033[1m cross validation \033[0m",
-            f"・ The \033[1m cross validation \033[0m is done!!!",
-            0.05
-        ).start()
+            f'・ Running \033[1m cross validation \033[0m',
+            f'・ The \033[1m cross validation \033[0m is done!!!',
+            0.05,
+            ).start()
     for i in range(n_size):
+        # Define kfold for running cross_validation
         kfold = KFold(n_splits=10, shuffle=True, random_state=i)
-        # For decision tree
+        # For each machine learning model
+        # Decision tree
         decision_tree = DecisionTreeClassifier(criterion='log_loss', min_samples_leaf=15, min_samples_split=25, splitter='random')
         scores_tree = cross_val_score(decision_tree, X_data, y_data, cv=kfold)
         results_tree.append(scores_tree.mean())
-        # For random forest   
+        # Random forest   
         random_forest = RandomForestClassifier(n_estimators=40,  criterion='gini', min_samples_leaf=1, min_samples_split=10)
         scores_random_forest = cross_val_score(random_forest, X_data, y_data, cv=kfold)
         results_random_forest.append(scores_random_forest.mean())
-        # For svc
-        svm = SVC(kernel = 'rbf', C = 100.0, tol=0.001)
-        scores_svm = cross_val_score(svm, X_data, y_data, cv=kfold)
-        results_svm.append(scores_svm.mean())
-    loader.stop()
+        # SVC
+        svc = SVC(kernel = 'rbf', C = 100.0, tol=0.001)
+        scores_svc = cross_val_score(svc, X_data, y_data, cv=kfold)
+        results_svc.append(scores_svc.mean())
+    loader.stop() # End loader -> Stop loading animation
     # results = [results_tree, results_random_forest]
-    results = [results_tree, results_random_forest, results_svm]
+    results = [results_tree, results_random_forest, results_svc]
     return results
