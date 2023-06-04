df4f513d44bf937434e49babe87eed0286182d22
romulolassoares <romulosoares@ice.ufjf.br>
2023-06-04 09:28:03 -0300

    integration01

--- src/old_main.py
+++ src/old_main.py
@@ -3,36 +3,24 @@
 import sys
 import pickle
 import numpy as np
-from pycaret import classification
 from tabulate import tabulate
 import statistics
 import time
-import pandas as pd
-
-from pycaret.classification import *
-
 from utils.cross_validation_utils import cross_validation
 from utils.find_best_agm_utils import find_best_agm
 from utils.teste_tukey_utils import tukey_test
 from utils.grid_search_utils import config_selector
-
-from utils.neural_network_linear_regression_utils_1 import foo
 # from utils.random_search_utils import config_selector
 agms = {0:'tree', 1:'random_forest', 2:'svc'}
 
 # Define pickle file squeme
-def read_file_pkl(file: str):
+def read_file(file: str):
     with open(file, 'rb') as f:
         # X_train, X_test, y_train, y_test = pickle.load(f)
         X_train, y_train, X_test, y_test = pickle.load(f)
     X_data = np.concatenate((X_train, X_test), axis=0)
     y_data = np.concatenate((y_train, y_test), axis=0)
     return X_data, y_data
-
-# Read a csv file and create a dataframe
-def read_file_csv(file: str):
-    data = pd.read_csv(file, index_col=None)
-    return data
 
 def print_table(results: list, agms: dict):
     result_t = []
@@ -44,7 +32,7 @@ def print_table(results: list, agms: dict):
     print("***********************************")
     
 def menu():
-    print("--- MENU ---")
+    print("\n\033[1m--- MENU ---\033[0m")
     print("0: Melhor algoritmo")
     print("1: Tabela com as médias")
     option = int(input("Select: "))
@@ -59,8 +47,8 @@ def main(file_path: str):
     # start = time.time()
     # print(f"Running for the dataset {file_path}")
     # # Get X and y values from file
-    # X_data, y_data = read_file_pkl(file_path)
-    # config_selector(X_data, y_data)
+    X_data, y_data = read_file(file_path)
+    config_selector(X_data, y_data)
     # # Exacute cross validation between agms
     # results = cross_validation(X_data, y_data)
     # # Using tukey test to find the best agm
@@ -71,48 +59,6 @@ def main(file_path: str):
     # # op = menu()
     # # foo_helper[op](results, agms)
     # # find_best_agm(results, agms)
-    target_position = -1
-
-    data = pd.read_csv(file_path)
-
-    target: str = str(data.columns[target_position])
-
-    s = ClassificationExperiment()
-    s = setup(
-            data=data, # Dataset
-            target=target, # Target Column
-            train_size=0.8, # Proportion of the dataset to be used to training and validation
-            numeric_imputation='knn', # Strategy for numerical columns (at missing values)
-            categorical_imputation='mode', # Strategy for categorical columns (at missing values)
-            html=False, # Prevents runtime display at command line
-            verbose=False,
-            fold_strategy='kfold',
-            fold=2,
-            session_id=None, # Controls the randomness of experiment
-            n_jobs=-1 # How much jobs will running paralel
-            )
-
-    # Select the best model
-    best_model = s.compare_models(
-            turbo=False,
-            sort='Recall',
-            #fold=5
-            )
-
-    # Tunning the best model
-    tunned_model = s.tune_model(
-            best_model,
-            n_iter=50,
-            search_library='scikit-learn',
-            search_algorithm='random',
-            optimize='Recall'
-            )
-
-    print(tunned_model)
-    
-    foo(s.X_train,s.y_train, s.X_test, s.y_test)
-
-
 
 if __name__ == "__main__":
     try:
